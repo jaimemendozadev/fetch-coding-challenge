0fc50a8d794a0baaf42da16916b4db10913a8454
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-02-04 14:20:21 -0800

    Remove useRefreshToken from HomePage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,15 @@
 'use client';
 import { ReactNode } from 'react';
+import { useGetUser, } from '@/utils/hooks';
 
 export default function HomePage(): ReactNode {
-  return <h1>Home Page 🏡</h1>;
+  const user = useGetUser();
+
+  console.log('user in HomePage 🏡 ', user);
+
+  return (
+    <div>
+      <h1>Home Page 🏡</h1>
+    </div>
+  );
 }
--- utils/hooks/index.ts
+++ utils/hooks/index.ts
@@ -1,2 +1,1 @@
-export * from './useRefreshToken';
 export * from './useGetUser';
