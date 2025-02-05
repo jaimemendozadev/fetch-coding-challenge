3a4fa66b08ab71e6064a55d571100391ee943a05
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-02-05 09:31:22 -0800

    Move BreedDropdown to SearchForm

--- components/searchform/index.tsx
+++ components/searchform/index.tsx
@@ -1,7 +1,16 @@
 'use client';
 import { useState, ReactNode } from 'react';
-import { Input, Form } from '@heroui/react';
-import { BreedDropdown } from './breeddropdown';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  SharedSelection,
+  Input,
+  Form
+} from '@heroui/react';
+import { DOG_BREEDS } from '@/utils';
 import { InputEvent } from '@/utils/ts';
 
 interface FormState {
@@ -17,6 +26,20 @@ export const SearchForm = (): ReactNode => {
     zipCodes: ''
   });
 
+  const [selectedBreeds, updateSelectedBreeds] = useState<SharedSelection>(
+    new Set(['Affenpinscher'])
+  );
+
+  // See Dev Note #1
+  const selectedValue = useMemo(() => {
+    const baseSelections = Array.from(selectedBreeds);
+    const trimmedSelection = baseSelections.slice(0, 1).join(', ').trim();
+
+    return baseSelections.length > 1
+      ? `${trimmedSelection}...`
+      : trimmedSelection;
+  }, [selectedBreeds]);
+
   const handleChange = (evt: InputEvent): void => {
     const { id } = evt.target;
 
@@ -24,6 +47,8 @@ export const SearchForm = (): ReactNode => {
 
     updateFormState((prev) => ({ ...prev, ...update }));
   };
+
+  const handleSubmit = async (evt: SubmitEvent): Promise<void> => {};
 
   return (
     <Form className="max-w-[80%] p-6 mx-auto flex flex-row justify-around items-center space-x-4 border-2 border-[#DF2A87] rounded-md">
@@ -54,7 +79,40 @@ export const SearchForm = (): ReactNode => {
         value={formState.maxAge}
         onChange={handleChange}
       />
-      <BreedDropdown />
+      <Dropdown>
+        <DropdownTrigger>
+          <Button className="capitalize" variant="bordered">
+            {selectedValue}
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu
+          className="max-h-[50vh] overflow-y-scroll" // See Dev Note #2
+          disallowEmptySelection
+          aria-label="Multiple selection example"
+          closeOnSelect={false}
+          selectedKeys={selectedBreeds}
+          selectionMode="multiple"
+          variant="flat"
+          onSelectionChange={updateSelectedBreeds}
+        >
+          {DOG_BREEDS.map((breedType) => (
+            <DropdownItem key={breedType}>{breedType}</DropdownItem>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
     </Form>
   );
 };
+
+/******************************************** 
+   * Notes
+   ******************************************** 
+
+   1) Had to cut off the selected breeds in the <Button> selectedValue
+      because it was breaking the layout. For now, this solution
+      will have to do.
+   
+   2) DropdownMenu clips DropdownItems if more the 80+.
+      Found solution at: https://github.com/heroui-inc/heroui/issues/3244#issuecomment-2173189338
+
+  */
