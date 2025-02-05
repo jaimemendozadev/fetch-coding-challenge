ef10d5a0c305013792cd4156d44720808590e817
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-02-05 04:36:02 -0800

    Temporarily comment out code to test Vercel deployment
    Apparently, it you leave unused dependencies in your code,
    the Vercel build breaks. 🤔🤷🏽

--- components/searchform/index.tsx
+++ components/searchform/index.tsx
@@ -1,16 +1,16 @@
 'use client';
-import { useState, useContext, ReactNode } from 'react';
+// import { useState, useContext, ReactNode } from 'react';
 import { BreedDropdown } from './breeddropdown';
 
 export const SearchForm = () => {
-  const [zipArray, updateZipArray] = useState<number[]>([]);
-  const [ageRange, updateAgeRange] = useState<{
-    min: number | null;
-    max: number | null;
-  }>({ min: null, max: null });
+//   const [zipArray, updateZipArray] = useState<number[]>([]);
+//   const [ageRange, updateAgeRange] = useState<{
+//     min: number | null;
+//     max: number | null;
+//   }>({ min: null, max: null });
 
   return (
-    <form>
+    <form className="h-[300px] border border-orange-800">
       <BreedDropdown />
     </form>
   );
