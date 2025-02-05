869e95654fccf2d3ff124f6264825d6df72d6b3a
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-02-05 06:46:27 -0800

    Add formState & onChange to SearchForm
    Discovered we need to fix BreedDropdown issue when
    lots of breeds are selected and it breaks the layout.

--- components/searchform/index.tsx
+++ components/searchform/index.tsx
@@ -1,34 +1,58 @@
 'use client';
-import { useState, useContext, ReactNode } from 'react';
-import { Input, Form, Button } from '@heroui/react';
+import { useState, ReactNode } from 'react';
+import { Input, Form } from '@heroui/react';
 import { BreedDropdown } from './breeddropdown';
+import { InputEvent } from '@/utils/ts';
 
-export const SearchForm = () => {
-  const [zipArray, updateZipArray] = useState<number[]>([]);
-  const [ageRange, updateAgeRange] = useState<{
-    min: number | null;
-    max: number | null;
-  }>({ min: null, max: null });
+interface FormState {
+  minAge: string;
+  maxAge: string;
+  zipCodes: string;
+}
+
+export const SearchForm = (): ReactNode => {
+  const [formState, updateFormState] = useState<FormState>({
+    minAge: '',
+    maxAge: '',
+    zipCodes: ''
+  });
+
+  const handleChange = (evt: InputEvent): void => {
+    const { id } = evt.target;
+
+    const update = { [id]: evt.target.value.trim() };
+
+    updateFormState((prev) => ({ ...prev, ...update }));
+  };
 
   return (
     <Form className="max-w-[80%] m-auto flex flex-row justify-around items-center space-x-4 border border-orange-800">
       <Input
+        id="zipCodes"
         className="w-[20%]"
         label="Zip Code"
         placeholder="Zip Codes (separated by comma)"
         type="text"
+        value={formState.zipCodes}
+        onChange={handleChange}
       />
       <Input
+        id="minAge"
         className="w-[20%]"
         label="Min Age"
         placeholder="Enter a Minimum Dog Age"
         type="text"
+        value={formState.minAge}
+        onChange={handleChange}
       />
       <Input
+        id="maxAge"
         className="w-[20%]"
         label="Max Age"
         placeholder="Enter a Maximum Dog Age"
         type="text"
+        value={formState.maxAge}
+        onChange={handleChange}
       />
       <BreedDropdown />
     </Form>
