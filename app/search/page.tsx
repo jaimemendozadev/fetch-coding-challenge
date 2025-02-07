e19c3408f196d8290a3b579423aae5d50b08d7ec
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-02-07 04:43:20 -0800

    Add formatSearchShape function to save search params in Store

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -10,7 +10,7 @@ import {
 import { useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
 import { SearchForm } from '@/components/searchform';
-import { BASE_URL, makeBackEndRequest } from '@/utils';
+import { BASE_URL, formatSearchShape, makeBackEndRequest } from '@/utils';
 import { StoreContext } from '@/utils/store';
 import { HTTP_METHODS } from '@/utils/ts';
 import { useSearchParams } from 'next/navigation';
@@ -23,26 +23,14 @@ const BASE_SEARCH_URL = `${BASE_URL}/dogs/search?`;
 function SearchPage(): ReactNode {
   const router = useRouter();
   const { store, updateStore } = useContext(StoreContext);
-  const queryParams = useSearchParams();
-
   const prevRendered = useRef(false);
+
+  const queryParams = useSearchParams();
 
   const ageMin = queryParams.get('ageMin');
   const ageMax = queryParams.get('ageMax');
   const zipCodes = queryParams.get('zipCodes');
   const breeds = queryParams.get('breeds');
-
-  console.log('ageMin ', ageMin);
-  console.log('\n');
-
-  console.log('ageMax ', ageMax);
-  console.log('\n');
-
-  console.log('zipCodes ', zipCodes);
-  console.log('\n');
-
-  console.log('breeds ', breeds);
-  console.log('\n');
 
   const handleSearchRedirect = (frontendURL: string) => {
     router.push(frontendURL);
@@ -61,14 +49,66 @@ function SearchPage(): ReactNode {
         const res = await makeBackEndRequest(payload, true, updateStore);
 
         console.log('res in SearchForm ', res);
+        console.log('\n');
+
+        const updateSearch = formatSearchShape(
+          ageMin,
+          ageMax,
+          zipCodes,
+          breeds
+        );
+
+        // See Dev Note #1
+        if (updateStore) {
+          updateStore((prev) => ({ ...prev, ...{ search: updateSearch } }));
+        }
       } catch (error) {
         // TODO: Handle in telemetry.
         console.log('Error in /search page makeSearchRequest hook: ', error);
         console.log('\n');
       }
     },
-    [updateStore]
+    [ageMax, ageMin, breeds, updateStore, zipCodes]
   );
+
+  const getSearchUrlString = useCallback(() => {
+    console.log('ageMin ', ageMin);
+    console.log('\n');
+
+    console.log('ageMax ', ageMax);
+    console.log('\n');
+
+    console.log('zipCodes ', zipCodes);
+    console.log('\n');
+
+    console.log('breeds ', breeds);
+    console.log('\n');
+
+    let searchQueryString = '';
+
+    if (ageMin !== null) {
+      searchQueryString = `ageMin=${ageMin}`;
+    }
+
+    if (ageMax !== null) {
+      searchQueryString = `${searchQueryString}&ageMax=${ageMax}`;
+    }
+
+    if (zipCodes !== null) {
+      searchQueryString = `${searchQueryString}&zipCodes=${zipCodes}`;
+    }
+
+    if (breeds !== null) {
+      searchQueryString = `${searchQueryString}&breeds=${breeds}`;
+    }
+
+    const searchURL =
+      searchQueryString.length === 0
+        ? BASE_SEARCH_URL
+        : `${BASE_SEARCH_URL}${searchQueryString}`;
+
+    return searchURL;
+  }, [ageMax, ageMin, breeds, zipCodes]);
 
   useEffect(() => {
     if (!store.user) {
@@ -80,64 +120,20 @@ function SearchPage(): ReactNode {
     }
   }, [router, store.user]);
 
-  // This hook should handle all subsequent makeSearchRequests
-  // after the page has had its first initial render.
+  // See Dev Note #2
   useEffect(() => {
-    let searchQueryString = '';
-
-    if (ageMin !== null) {
-      searchQueryString = `ageMin=${ageMin}`;
-    }
-
-    if (ageMax !== null) {
-      searchQueryString = `${searchQueryString}&ageMax=${ageMax}`;
-    }
-
-    if (zipCodes !== null) {
-      searchQueryString = `${searchQueryString}&zipCodes=${zipCodes}`;
-    }
-
-    if (breeds !== null) {
-      searchQueryString = `${searchQueryString}&breeds=${breeds}`;
-    }
-
-    const searchURL =
-      searchQueryString.length === 0
-        ? BASE_SEARCH_URL
-        : `${BASE_SEARCH_URL}${searchQueryString}`;
+    const searchURL = getSearchUrlString();
 
     if (prevRendered.current === true) {
       console.log('firing makeSearchRequest in secondary hook');
       console.log('\n');
       makeSearchRequest(searchURL);
     }
-  }, [ageMax, ageMin, breeds, makeSearchRequest, updateStore, zipCodes]);
+  }, [getSearchUrlString, makeSearchRequest, updateStore]);
 
-  // This hook handles initial rendering of <SearchPage />, whether
-  // user is coming from <HomePage /> or directly goes to <SearchPage />.
+  // See Dev Note #3
   useEffect(() => {
-    let searchQueryString = '';
-
-    if (ageMin !== null) {
-      searchQueryString = `ageMin=${ageMin}`;
-    }
-
-    if (ageMax !== null) {
-      searchQueryString = `${searchQueryString}&ageMax=${ageMax}`;
-    }
-
-    if (zipCodes !== null) {
-      searchQueryString = `${searchQueryString}&zipCodes=${zipCodes}`;
-    }
-
-    if (breeds !== null) {
-      searchQueryString = `${searchQueryString}&breeds=${breeds}`;
-    }
-
-    const searchURL =
-      searchQueryString.length === 0
-        ? BASE_SEARCH_URL
-        : `${BASE_SEARCH_URL}${searchQueryString}`;
+    const searchURL = getSearchUrlString();
 
     if (prevRendered.current === false) {
       console.log('firing makeSearchRequest in initial hook');
@@ -155,7 +151,7 @@ function SearchPage(): ReactNode {
   );
 }
 
-// See Dev Note #1
+// See Dev Note #4
 export default function WrappedSearchPage(): ReactNode {
   return (
     <Suspense>
@@ -167,8 +163,18 @@ export default function WrappedSearchPage(): ReactNode {
 /******************************************** 
    * Notes
    ******************************************** 
+   1) This handles the case where maybe the user saved the web browser
+      URL from a previous session and directly came to the /search page
+      without having come from the home page. We have to save the search 
+      parameters so the user can continue searching.
 
-   1) Per Vercel Deploy Error logs:
+   2) This hook should handle all subsequent makeSearchRequests
+      after the page has had its first initial render.
+
+   3) This hook handles initial rendering of <SearchPage />, whether the
+      user is coming from <HomePage /> or directly goes to <SearchPage />.
+
+   4) Per Vercel Deploy Error logs:
       
       useSearchParams() should be wrapped in a suspense boundary at page "/search". 
       Read more: https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout
--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
 import { Dispatch, SetStateAction } from 'react';
 import { StoreShape } from '@/utils/store';
-import { RequestPayload } from './ts';
+import { RequestPayload, SearchShape } from './ts';
 
 export const BASE_URL = 'https://frontend-take-home-service.fetch.com';
 export const AUTH_URL = `${BASE_URL}/auth/login`;
@@ -12,15 +12,44 @@ interface CallAPIError {
   statusText: string;
 }
 
-// 🔹 Type guard to check if the response is an error
-const isCallAPIError = (response: any): response is CallAPIError => {
-  return response && typeof response === 'object' && 'error' in response;
-};
-
 // See Dev Note #1
 export const validateEmail = (email: string): boolean => {
   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return regex.test(email);
+};
+
+export const formatSearchShape = (
+  ageMin: string | null,
+  ageMax: string | null,
+  zipCodes: string | null,
+  breeds: string | null
+): SearchShape => {
+  const searchShape: SearchShape = {
+    ageMin: '',
+    ageMax: '',
+    zipCodes: '',
+    breeds: new Set([])
+  };
+
+  if (ageMin !== null) {
+    searchShape['ageMin'] = ageMin;
+  }
+
+  if (ageMax !== null) {
+    searchShape['ageMax'] = ageMax;
+  }
+
+  if (zipCodes !== null) {
+    searchShape['zipCodes'] = zipCodes;
+  }
+
+  if (typeof breeds === 'string' && breeds.length > 0) {
+    const breedArray = breeds.trim().split(',');
+
+    searchShape['breeds'] = new Set(breedArray);
+  }
+
+  return searchShape;
 };
 
 export const reauthenticateUser = async (
@@ -100,6 +129,11 @@ const callAPI = async <T>(
     console.error(`❌ Network Error: Unable to reach ${apiURL}`, error);
     return { error: true, status: 500, statusText: 'Network Error' };
   }
+};
+
+// 🔹 Type guard to check if the response is an error
+const isCallAPIError = (response: any): response is CallAPIError => {
+  return response && typeof response === 'object' && 'error' in response;
 };
 
 export const makeBackEndRequest = async <T>(
