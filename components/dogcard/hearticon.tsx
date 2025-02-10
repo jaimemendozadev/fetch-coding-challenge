75484c945bb9f2fedac670f0df7302c1a01233bf
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-02-10 09:19:57 -0800

    Fix slightly disjointed HeartIcon

--- components/dogcard/hearticon.tsx
+++ components/dogcard/hearticon.tsx
@@ -1,19 +1,20 @@
 'use client';
 export const HeartIcon = ({
-  fill = 'black',
+  fill = 'none',
+  stroke = 'black',
   size = 24,
   height = 24,
   width = 24,
   ...props
 }: {
-  fill: string;
-  size: number;
-  height: number;
-  width: number;
+  fill?: string;
+  stroke?: string;
+  size?: number;
+  height?: number;
+  width?: number;
 }) => {
   return (
     <svg
-      fill={fill}
       height={size || height || 24}
       width={size || width || 24}
       viewBox="0 0 24 24"
@@ -21,8 +22,9 @@ export const HeartIcon = ({
       {...props}
     >
       <path
-        d="M12 21s-1-.45-1-1.55c0-.76.31-1.48.88-2.04C14.22 15.14 18 12.04 18 8.5c0-2.5-2-4.5-4.5-4.5-1.54 0-3.04.79-3.89 2.04C8.04 4.79 6.54 4 5 4 2.5 4 .5 6 .5 8.5c0 3.54 3.78 6.64 6.12 8.91.57.56.88 1.28.88 2.04C8 20.55 7 21 7 21"
-        stroke={fill}
+        d="M12 21s-6.5-4.3-9-8.3C1 9.2 2.5 5 6 3.8c2.2-.7 4.7 0 6 2 1.3-2 3.8-2.7 6-2 3.5 1.2 5 5.4 3 8.9-2.5 4-9 8.3-9 8.3Z"
+        fill={fill}
+        stroke={stroke}
         strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
